9feec31731797e16b37c1185e2345ad6c816d916
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-10-19 08:02:21 -0700

    init

---
+++ src/main/java/com/capstone/countertop/CountertopApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.countertop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountertopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountertopApplication.class, args);
+    }
+
+}
