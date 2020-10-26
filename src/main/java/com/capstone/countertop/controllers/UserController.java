a884d2968491d0d39fc07b550bf0d09667548337
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-10-26 11:04:01 -0500

    updated pom xml and working on controllers

--- src/main/java/com/capstone/countertop/controllers/UserController.java
+++ src/main/java/com/capstone/countertop/controllers/UserController.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.capstone.countertop.models.User;
 import com.capstone.countertop.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 
 @Controller
 public class UserController {
-//    private final UserRepository userDao;
-//    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("users/register")
     public String registerForm() {
