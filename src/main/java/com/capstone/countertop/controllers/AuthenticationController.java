fb9a55b8914b96e49e038761b35ec4725db741b5
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-10-26 12:06:58 -0700

    Recipe Controller & Authentication Controller fleshed out

--- src/main/java/com/capstone/countertop/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/countertop/controllers/AuthenticationController.java
@@ -1,23 +1,40 @@
 package com.capstone.countertop.controllers;
 
+import com.capstone.countertop.models.User;
+import com.capstone.countertop.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @GetMapping("/login")
     public String loginForm() {
         return "login";
     }
 
     @GetMapping("/register")
-    public String registerForm() {
+    public String registerForm(Model model) {
+        model.addAttribute("user", new User());
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser() {
+    public String registerUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userRepository.save(user);
         return "redirect:/login";
     }
 }
--- src/main/java/com/capstone/countertop/controllers/RecipeController.java
+++ src/main/java/com/capstone/countertop/controllers/RecipeController.java
@@ -1,44 +1,85 @@
 package com.capstone.countertop.controllers;
 
+import com.capstone.countertop.models.Recipe;
+import com.capstone.countertop.models.User;
+import com.capstone.countertop.repositories.RecipeRepository;
+import com.capstone.countertop.repositories.UserRepository;
+import com.capstone.countertop.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
+    private final RecipeRepository recipeRepository;
+    private final UserRepository userRepository;
+    private final EmailService emailService;
+
+    public RecipeController(RecipeRepository recipeRepository, UserRepository userRepository, EmailService emailService) {
+        this.recipeRepository = recipeRepository;
+        this.userRepository = userRepository;
+        this.emailService = emailService;
+    }
+
     @GetMapping("/recipes")
-    public String showRecipes() {
+    public String showRecipes(Model model) {
+        model.addAttribute("recipes", recipeRepository.findAll());
         return "recipes/index";
     }
 
     @GetMapping("/recipes/{id}")
-    public String showRecipe(@PathVariable long id) {
+    public String showRecipe(@PathVariable long id, Model model) {
+        model.addAttribute("recipe", recipeRepository.getOne(id));
         return "recipes/recipe";
     }
 
     @GetMapping("/recipes/delete/{id}")
     public String deleteRecipe(@PathVariable long id) {
-        return "recipes/delete";
+        Recipe recipe = recipeRepository.getOne(id);
+        if (recipe != null)
+            recipeRepository.delete(recipe);
+        return "redirect:/recipes";
     }
 
     @GetMapping("/recipes/create")
-    public String createRecipeForm() {
+    public String createRecipeForm(Model model) {
+        model.addAttribute("recipe",new Recipe());
         return "recipes/form";
     }
 
     @PostMapping("/recipes/create")
-    public String createRecipe() {
+    public String createRecipe(@ModelAttribute Recipe recipe) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        recipe.setUser(user);
+        recipeRepository.save(recipe);
+        emailService.prepareAndSend(recipe, "New Recipe Submitted!", "Congratulations on submitting your new recipe!");
         return "redirect:/recipes/";
     }
 
     @GetMapping("/recipes/edit/{id}")
-    public String editRecipeForm() {
+    public String editRecipeForm(@PathVariable long id, Model model) {
+        Recipe recipe = recipeRepository.getOne(id);
+        if (recipe == null)
+            return "redirect:/recipes/index";
+        model.addAttribute("recipe",recipe);
         return "recipes/form";
     }
 
+    // This needs to be fleshed out more once form is fleshed out.
     @PostMapping("/recipes/edit")
-    public String editRecipe() {
+    public String editRecipe(
+            @RequestParam(name="id") long id,
+            @RequestParam(name="title") String name,
+            @RequestParam(name="description") String description,
+            Model model) {
+        Recipe recipe = recipeRepository.getOne(id);
+        if (recipe == null)
+            return "redirect:/recipes/index";
+        recipe.setName(name);
+        recipe.setDescription(description);
+        recipeRepository.save(recipe);
         return "redirect:/recipes/";
     }
 
