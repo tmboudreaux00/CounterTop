dea2d7dc2564ac903253f4b5dae488bbb4de996a
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-10-26 10:43:36 -0500

    merging controllers and models

--- src/main/java/com/capstone/countertop/controllers/UserController.java
+++ src/main/java/com/capstone/countertop/controllers/UserController.java
@@ -3,9 +3,12 @@ package com.capstone.countertop.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.capstone.countertop.models.User;
+
 
 @Controller
 public class UserController {
+
     @GetMapping("users/register")
     public String registerForm() {
         return "users/register";
