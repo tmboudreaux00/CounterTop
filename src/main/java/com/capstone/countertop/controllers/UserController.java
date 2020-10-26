9140c15454b1d1a72c258d614d3e486d855ce900
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-10-26 14:10:24 -0500

    worked on user controller

--- src/main/java/com/capstone/countertop/controllers/UserController.java
+++ src/main/java/com/capstone/countertop/controllers/UserController.java
@@ -2,7 +2,10 @@ package com.capstone.countertop.controllers;
 
 import com.capstone.countertop.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.capstone.countertop.models.User;
 import com.capstone.countertop.repositories.UserRepository;
@@ -20,13 +23,23 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @GetMapping("user/{id}")
+    public String profilePage(@PathVariable long id, Model model){
+        model.addAttribute("user", userDao.getOne(id));
+        return "/profile";
+    }
+
     @GetMapping("users/register")
-    public String registerForm() {
-        return "users/register";
+    public String registerForm(Model model) {
+        model.addAttribute("user", new User());
+        return "/register";
     }
 
     @PostMapping("users/register")
-    public String registerUser() {
+    public String registerUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
         return "redirect:/login";
     }
 }
