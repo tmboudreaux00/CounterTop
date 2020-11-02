9c56956ac51ae8d8b6d1f4e3b1d3c49060809050
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-11-02 09:53:44 -0800

    getitng spoonacular via backend

--- src/main/java/com/capstone/countertop/controllers/SearchController.java
+++ src/main/java/com/capstone/countertop/controllers/SearchController.java
@@ -6,6 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
 @Controller
 public class SearchController {
     private final RecipeRepository recipeRepository;
@@ -16,7 +22,24 @@ public class SearchController {
 
     @GetMapping("/recipes/search")
     public String searchReturn(@RequestParam(name="searchTerm") String searchTerm, Model model) {
+        try {
+            get("https://api.spoonacular.com/food/products/search?query="+ searchTerm +"&apiKey=5af6024d199b481f99458dc8fc697543");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         model.addAttribute("recipes", recipeRepository.searchRecipesByTerm(searchTerm));
         return "recipes/index";
+    }
+
+    public void get(String uri) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
