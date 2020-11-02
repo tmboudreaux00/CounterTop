f82f5c4ea11286527df97aa6a651ac1245702e2b
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-11-02 09:36:35 -0800

    removed couple of imports

--- src/main/java/com/capstone/countertop/controllers/SearchController.java
+++ src/main/java/com/capstone/countertop/controllers/SearchController.java
@@ -4,8 +4,6 @@ import com.capstone.countertop.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
